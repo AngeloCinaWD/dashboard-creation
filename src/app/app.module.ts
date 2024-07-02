761652123e4b757de62c2441f096e86e02a48b5a
unknown <a.cina@procreatech.com>
2024-07-02 10:25:17 +0200

    per installare high chart utilizzare npm i --save angular-highcharts highcharts e poi cambiare la versione nel package.json a 13.0.1 e runnare npm install altrimenti come fa lui nel video si hanno problemi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { SalesByCategoryComponent } from './components/sales-by-category/sales-b
 import { LastFewTransactionsComponent } from './components/last-few-transactions/last-few-transactions.component';
 import { TopThreeProductsComponent } from './components/top-three-products/top-three-products.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ChartModule } from 'angular-highcharts';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
